16095985b2403ae1ba49625c1214706e40d827b8
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-10-05 15:19:23 -0300

    Partial bicubic code adaptation [2]

--- src/Backends/DRMBackend.cpp
+++ src/Backends/DRMBackend.cpp
@@ -3458,6 +3458,7 @@ namespace gamescope
 			bNeedsFullComposite |= bWasFirstFrame;
 			bNeedsFullComposite |= pFrameInfo->useFSRLayer0;
 			bNeedsFullComposite |= pFrameInfo->useNISLayer0;
+			bNeedsFullComposite |= pFrameInfo->useBICUBICLayer0;
 			bNeedsFullComposite |= pFrameInfo->blurLayer0;
 			bNeedsFullComposite |= bNeedsCompositeFromFilter;
 			bNeedsFullComposite |= !k_bUseCursorPlane && bDrewCursor;
